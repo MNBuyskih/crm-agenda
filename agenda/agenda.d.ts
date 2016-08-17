30e83f3afa9c14d11f1a58d9512326d6bcbfca2c
MNB <buyskih@gmail.com>
2016-08-17 11:45:22 +0300

    Add declaration

---
+++ agenda/agenda.d.ts
@@ -0,0 +1,26 @@
+declare module Agenda {
+    export let Agenda: IAgendaStatic;
+
+    export interface IAgendaOptions {
+        hourHeight: number;
+        onTimePick?: (start: number[], end: number[]) => void;
+        title: string;
+    }
+
+    interface IAgendaStatic {
+        new(container: JQuery, private options: IAgendaOptions): IAgendaInstance;
+    }
+
+    interface IAgendaInstance {
+        getStartTime(): number[];
+        getEndTime(): number[];
+        getCoordFromTime(hour: number, min: number): number;
+        setTitle(title: string): void;
+        setStartTime(hours: number, min: number): void;
+        setEndTime(hours: number, min: number): void;
+    }
+}
+
+declare interface JQuery {
+    Agenda(options: Agenda.IAgendaOptions): Agenda.IAgendaInstance;
+}
